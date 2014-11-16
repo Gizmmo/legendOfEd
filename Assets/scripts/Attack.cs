05e7b659f7539b2b42f8d280ec8fbb5b24b3dd39
Travis Scott <travisscott301@gmail.com>
2014-11-15 21:03:45 -0600

    Initial Commit

---
+++ Assets/scripts/Attack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+	
+	public float timeBetweenAttacks = 0.5f;
+	public GameObject sword;
+	public float swordYPosition;
+	public float swordXPosition;
+
+	private GameObject visibleSword;
+	private float timestamp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time >= timestamp)
+		{
+			Destroy (visibleSword);
+
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				Vector3 pos = transform.position;
+				pos.x += swordXPosition;
+				pos.y += swordYPosition;
+				visibleSword = Instantiate(sword, pos, transform.rotation) as GameObject;
+				timestamp = Time.time + timeBetweenAttacks;
+			}
+		}
+	}
+}
---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2(speed * moveX, speed * moveY);
+		rigidbody2D.MovePosition(rigidbody2D.position + movement * Time.deltaTime);
+	}
+}
